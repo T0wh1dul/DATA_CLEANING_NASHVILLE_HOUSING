28e5058f1121c0847310d9b18ab7525f0e0516b0
Towhidul Islam <tawheedul578@gmail.com>
2025-06-14 14:45:19 +0600

    Completed cleaning and transformation of Nashville housing data
    Finalizes the data cleaning and transformation process for the Nashville housing dataset. Key changes include:
    Standardized SaleDate format and added SaleDateConverted column.
    Populated missing PropertyAddress values using ParcelID joins.
    Extracted and separated PropertyAddress into PropretySplitAddress and PropretySplitCity.
    Extracted and separated OwnerAddress into OwnerSplitAddress, OwnerSplitCity, and OwnerSplitState.
    Standardized SoldAsVacant values from 'Y'/'N' to 'Yes'/'No'.
    Removed duplicate records based on key identifying columns.
    Dropped original uncleaned/redundant columns (OwnerAddress, TaxDistrict, PropertyAddress, SaleDate).

--- DATA_CLEANING_NASHVILLE_HOUSING.sql
+++ DATA_CLEANING_NASHVILLE_HOUSING.sql
@@ -1,5 +1,3 @@
-select * 
-from PortfolioProject.dbo.Housing_data
 
 -- Standardize the date format 
 select SaleDateConverted, Convert(Date,SaleDate)
@@ -66,3 +64,120 @@ set PropretySplitCity= SUBSTRING( PropertyAddress, CHARINDEX(',', PropertyAddres
 
 select *
 from PortfolioProject.dbo.Housing_data
+
+			----------  OwnerAddress
+
+select OwnerAddress
+from PortfolioProject.dbo.Housing_data
+
+select 
+PARSENAME(replace(OwnerAddress,',','.'),3)
+,PARSENAME(replace(OwnerAddress,',','.'),2)
+,PARSENAME(replace(OwnerAddress,',','.'),1)
+from PortfolioProject.dbo.Housing_data
+
+
+
+--1
+Alter table PortfolioProject.dbo.Housing_data
+add OwnerSplitAddress NvArchar(255);
+
+update PortfolioProject.dbo.Housing_data
+set OwnerSplitAddress = PARSENAME(replace(OwnerAddress,',','.'),3)
+--2
+
+Alter table PortfolioProject.dbo.Housing_data
+add OwnerSplitCity  NvArchar(255);
+
+update PortfolioProject.dbo.Housing_data
+set OwnerSplitCity= PARSENAME(replace(OwnerAddress,',','.'),2)
+
+--3
+
+Alter table PortfolioProject.dbo.Housing_data
+add OwnerSplitState NvArchar(255);
+
+update PortfolioProject.dbo.Housing_data
+set OwnerSplitState = PARSENAME(replace(OwnerAddress,',','.'),1)
+
+
+select *
+from PortfolioProject.dbo.Housing_data
+
+----------------------------------------------------------------
+
+-- Changing Y and N to Yes and no in "Sold as vacant" Field
+
+
+--select distinct(SoldAsVacant), COUNT(SoldAsVacant)
+--From  PortfolioProject.dbo.Housing_data
+--group by SoldAsVacant
+--order by 2
+
+
+
+select SoldAsVacant,
+Case when SoldAsVacant = 'Y' then 'Yes'
+	 When SoldAsVacant = 'N' then 'No'
+	 else SoldAsVacant 
+	 end
+From  PortfolioProject.dbo.Housing_data
+
+
+update PortfolioProject.dbo.Housing_data
+set SoldAsVacant = Case when SoldAsVacant = 'Y' then 'Yes'
+	 When SoldAsVacant = 'N' then 'No'
+	 else SoldAsVacant 
+	 end
+
+
+	 -- for review
+		select distinct(SoldAsVacant), COUNT(SoldAsVacant)
+		From  PortfolioProject.dbo.Housing_data
+		group by SoldAsVacant
+		order by 2
+
+-----------------------------------------------------------------------------------
+
+
+----Removing Duplicates
+
+With RowNumCTE AS(
+select * ,
+	ROW_NUMBER() over( 
+	Partition by ParcelID,
+			 PropertyAddress,
+			 SalePrice,
+			 SaleDate,
+			 LegalReference
+			 Order by 
+			 UniqueID
+			 ) Row_num
+
+From  PortfolioProject.dbo.Housing_data
+
+)
+
+--delete
+--from RowNumCTE
+--where Row_num >1 
+ 
+select * 
+from RowNumCTE
+where Row_num >1 
+order by PropertyAddress
+
+
+
+-----------------------------------------------------------------------------
+
+-- Deleted unused columns -- 
+
+--select *
+--From  PortfolioProject.dbo.Housing_data
+
+alter table PortfolioProject.dbo.Housing_data
+drop column Owneraddress, TaxDistrict, PropertyAddress, SaleDate
+
+select *
+From  PortfolioProject.dbo.Housing_data
